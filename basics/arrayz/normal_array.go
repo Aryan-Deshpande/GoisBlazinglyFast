d906f09c9492f9655df8878742db994713eb3b48
Aryan Deshpande <deshpandearyan1@gmail.com>
2022-12-06 16:51:19 +0530

    arrays

---
+++ basics/arrayz/normal_array.go
@@ -0,0 +1,31 @@
+package arrayz
+
+import (
+	"fmt"
+	"reflect"
+) 
+
+func some(){
+	// this is a normal array
+	arr := []string {"hey","there","how","are","you"};
+
+	fmt.Println(reflect.TypeOf(arr));
+
+	ans:="" // prints the array
+
+	// to find the length of an array use // len(x) //
+	for i := 0; i < len(arr); i++ {
+
+		switch arr[i]{
+		case "you":
+			ans+=arr[i]+"?"
+		case "there":
+			ans+=arr[i]+", "
+		default:
+			ans+=arr[i]+" ";
+		}
+	}
+
+	fmt.Println(ans) // this prints // "hey there, how are you ?"
+
+}
---
+++ basics/arrayz/struct_array.go
@@ -0,0 +1,20 @@
+package arrayz
+
+import (
+	"fmt"
+
+)
+
+// normal struct along with arrays
+type life struct{
+	name string
+	jobs []string
+	houses int
+	children []string
+}
+
+func something(){
+	
+	fmt.Println("hi")
+
+}
